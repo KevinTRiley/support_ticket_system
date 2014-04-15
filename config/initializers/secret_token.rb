f475cb163b8f406d197279d339530abb295f806a
KevinTRiley <kevin.riley.dev@gmail.com>
2014-04-15 14:04:07 -0400

    Initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SupportTicketSystem::Application.config.secret_key_base = '7d5ff24dbac61529d8bc9430882aec5ad48dea5e50329ca196e118b7f2767c04b66b9e791ece939033e610ab12c127f7daa168e25ce569cf702906b638e29ade'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SupportTicketSystem::Application.config.session_store :cookie_store, key: '_support_ticket_system_session'
